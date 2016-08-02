d339baa7c2782bb3b647c9ea60a1baf6ba605569
Daniels <chantelle.daniels2@philips.com>
2016-08-02 14:53:38 +0100

    initial

---
+++ src/main/java/homepage/Application.java
@@ -0,0 +1,14 @@
+package homepage; /**
+ * Created by 310247318 on 02/08/2016.
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application{
+
+    public static void main(String[] args) throws Exception {
+        System.out.print("bbb");
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/homepage/MainController.java
@@ -0,0 +1,33 @@
+package homepage;
+
+/**
+ * Created by 310247318 on 02/08/2016.
+ */
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.stereotype.Controller;
+
+@ComponentScan
+@Controller
+public class MainController {
+
+    private final String test = "Hello";
+
+    @RequestMapping(value="/", method = RequestMethod.GET)
+    public String home(Model model) {
+
+        System.out.print("aaa");
+        return "mainPage";
+    }
+
+    @RequestMapping(value="/radiotherapy", method = RequestMethod.GET)
+    public String radiotherapy(Model model) {
+        System.out.print("ccc");
+        return "chart";
+    }
+
+}
